72ac8fd71a093a77014a4a6ba656211c054dd101
Kevin Gutowski <kgutowski@berkeley.edu>
2020-06-15 14:48:13 -0700

    initial commit

---
+++ src/my-command.js
@@ -0,0 +1,15 @@
+import sketch from 'sketch'
+// documentation: https://developer.sketchapp.com/reference/api/
+
+export default function() {
+  fetch("https://raw.githubusercontent.com/simple-icons/simple-icons/develop/icons/github.svg")
+    .then(response => {
+      return response.text()
+    })
+    .then(text => {
+      console.log(text)
+      // create symbol here from text
+      const group = sketch.createLayerFromData(stringResponse,'svg')
+    })
+    .catch(e => console.error(e))
+}
